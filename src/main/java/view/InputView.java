5b0e9c536289af014bf2822a2afaca970428c731
lalize <printf@kakao.com>
2019-12-20 23:32:22 +0900

    feat: 베팅 금액을 입력받는 기능 추가

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -6,9 +6,15 @@ public class InputView {
 	private static final Scanner SCANNER = new Scanner(System.in);
 	private static final String NAME_DELIMITER = ",";
 	private static final String GET_PLAYER_NAMES_MESSAGE = "게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)";
+	private static final String GET_BETTING_MONEY_FORMAT = "%s의 베팅 금액은?\n";
 
 	public static String[] getPlayerNames() {
 		System.out.println(GET_PLAYER_NAMES_MESSAGE);
 		return SCANNER.nextLine().split(NAME_DELIMITER);
+	}
+
+	public static double getBettingMoney(String name) {
+		System.out.printf(GET_BETTING_MONEY_FORMAT, name);
+		return Double.parseDouble(SCANNER.nextLine());
 	}
 }
