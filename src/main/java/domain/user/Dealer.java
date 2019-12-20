99267131e09e160c71a2c9aa888222979d2ef092
lalize <printf@kakao.com>
2019-12-20 23:49:11 +0900

    refactor: Dealer 클래스가 Player 클래스를 상속받도록 수정

--- src/main/java/domain/user/Dealer.java
+++ src/main/java/domain/user/Dealer.java
@@ -1,21 +1,13 @@
 package domain.user;
 
-import domain.card.Card;
-
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * 게임 딜러를 의미하는 객체
  */
-public class Dealer {
-    private final List<Card> cards = new ArrayList<>();
+public class Dealer extends Player {
+    private static final String NAME = "딜러";
+    private static final double BETTING_MONEY = 0;
 
-    public Dealer() {}
-
-    public void addCard(Card card) {
-        cards.add(card);
+    public Dealer() {
+        super(NAME, BETTING_MONEY);
     }
-
-    // TODO 추가 기능 구현
 }
