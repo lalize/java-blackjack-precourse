3d5fca18a0b96965c0a67303ba4492d99c2ebd61
lalize <printf@kakao.com>
2019-12-23 00:21:53 +0900

    fix: 커밋 누락, 카드를 추가로 입력받는 기능 추가

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -7,6 +7,7 @@ public class InputView {
 	private static final String NAME_DELIMITER = ",";
 	private static final String GET_PLAYER_NAMES_MESSAGE = "게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)";
 	private static final String GET_BETTING_MONEY_FORMAT = "%s의 베팅 금액은?\n";
+	private static final String GET_WHETHER_ADD_CARD_FORMAT = "%s는 카드를 더 받겠습니까?(예는 y, 아니오는 n)\n";
 
 	public static String[] getPlayerNames() {
 		System.out.println(GET_PLAYER_NAMES_MESSAGE);
@@ -16,5 +17,10 @@ public class InputView {
 	public static double getBettingMoney(String name) {
 		System.out.printf(GET_BETTING_MONEY_FORMAT, name);
 		return Double.parseDouble(SCANNER.nextLine());
+	}
+
+	public static String getWhetherAddCard(String name) {
+		System.out.printf(GET_WHETHER_ADD_CARD_FORMAT, name);
+		return SCANNER.nextLine();
 	}
 }
